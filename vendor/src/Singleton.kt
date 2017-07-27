f11c7cf13e305ab0fb16d5628cdad2ae79d583cd
azisuazusa <azis.abdul.bachar@gmail.com>
2017-07-27 20:05:28 +0700

    something change

--- vendor/src/Singleton.kt
+++ vendor/src/Singleton.kt
@@ -5,5 +5,5 @@ class Singleton private constructor() {
     companion object {
         val instance: Singleton by lazy { Singleton() }
     }
-    var props:String? = null
+    var prop:String? = null
 }
