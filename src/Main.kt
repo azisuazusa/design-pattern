0109101d3d8d175bc461d2d66757b1895ecda009
azisuazusa <azis.abdul.bachar@gmail.com>
2017-07-27 17:43:42 +0700

    try again

--- src/Main.kt
+++ src/Main.kt
@@ -6,10 +6,7 @@ import DecoratorPattern.RunnerDecorator
  * Created by azisuazusa on 7/22/17.
  */
 fun main(args: Array<String>) {
-    val singleton1 = Singleton.instance
-    singleton1.prop = "111111"
-    val singleton2 = Singleton.instance
-    print(singleton2.prop)
+    
 }
 
 fun builderPattern() {
--- vendor/src/Singleton.kt
+++
@@ -1,9 +0,0 @@
-/**
- * Created by azisuazusa on 7/27/17.
- */
-class Singleton private constructor() {
-    companion object {
-        val instance: Singleton by lazy { Singleton() }
-    }
-    var prop:String? = null
-}
