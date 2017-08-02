ae1495394b8abc24b7f0a04a73051d36dfd6bdaf
azisuazusa <azis.abdul.bachar@gmail.com>
2017-08-02 18:24:41 +0700

    Add Subject

--- src/ObserverPattern/Subject.kt
+++ src/ObserverPattern/Subject.kt
@@ -4,7 +4,18 @@ package ObserverPattern
  * Created by azisuazusa on 7/22/17.
  */
 class Subject {
-    val observers: List<Observer> = ArrayList()
-    private var state: Int = 0
+    val observers: MutableList<Observer> = ArrayList()
 
+    fun setColor(color: String) {
+        notifyAllObservers(color)
+    }
+    fun registerObserver(observer: Array<Observer>) {
+        observers.addAll(observer)
+    }
+
+    fun notifyAllObservers(color: String) {
+        for (observer in observers) {
+            observer.update(color)
+        }
+    }
 }
